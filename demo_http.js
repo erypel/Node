1685477243c853862dde6d00dc1a92bfe2c3e574
Evan <evan.rypel@gmail.com>
2018-11-03 18:49:20 -0700

    practiced creating a web server, adding an HTTP Header, reading a query string, and parsing the query string

---
+++ demo_http.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var url = require('url');
+
+//create a server object:
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'}); 
+	var q = url.parse(req.url, true).query;
+	var txt = q.year + " " + q.month;
+	//res.write(req.url); //write the part of the url after the domain name
+	res.end(txt); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+
+	
